616d2c99bfcad124eb0533f6a459fd90fead6e99
Jakub <jakubcihanek@gmail.com>
2024-09-23 23:38:30 +0200

    added serial and webserial toggle

--- src/Config.h
+++ src/Config.h
@@ -2,6 +2,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define DEBUG_ACTIVE // Define DEBUG_ACTIVE globally here
+#define DEBUG_ACTIVE
+
+#define SERIAL_DEBUG
+
+#define WEBSERIAL_DEBUG
 
 #endif // CONFIG_H
