6e45e379561dec031d1d3820813df0cf6ac7031b
Jakub <jakubcihanek@gmail.com>
2024-09-23 23:38:55 +0200

    serial webserial I/O

--- src/DebugEsp.cpp
+++ src/DebugEsp.cpp
@@ -8,42 +8,59 @@ DebugEsp::DebugEsp() {}
 // Initialize the logger with the baud rate
 void DebugEsp::begin(unsigned long baudRate)
 {
+#ifdef SERIAL_DEBUG
     Serial.begin(baudRate);
     delay(2000); // Give some time for Serial to start
+#endif
+
+#ifdef WEBSERIAL_DEBUG
+
+#endif
 }
 
 // Print without new line for errors
 void DebugEsp::debE(const char *message, bool newline)
 {
+#ifdef SERIAL_DEBUG
     printLog(ERROR, message, newline);
+#endif // SERIAL_DEBUG
 }
 
 // Log info with new line
 void DebugEsp::debI(const char *message, bool newline)
 {
+#ifdef SERIAL_DEBUG
     printLog(INFO, message, newline);
+#endif // SERIAL_DEBUG
 }
 
 // Log warnings with new line
 void DebugEsp::debW(const char *message, bool newline)
 {
+#ifdef SERIAL_DEBUG
     printLog(WARNING, message, newline);
+#endif // SERIAL_DEBUG
 }
 
 void DebugEsp::debActivityIndicator()
 {
+#ifdef SERIAL_DEBUG
     const char activityChars[] = {'|', '/', '-', '\\'};
-    Serial.print("\b");                         // Move cursor back to overwrite the previous character
-    Serial.print(activityChars[activityState]); // Print the current activity character
-    activityState = (activityState + 1) % 4;    // Update the state to the next character
+    Serial.print("\b");
+    Serial.print(activityChars[activityState]);
+    activityState = (activityState + 1) % 4;
+#endif // SERIAL_DEBUG
 }
 
 void DebugEsp::debActivityIndicatorStop()
 {
-    Serial.println(); // Update the state to the next character
+#ifdef SERIAL_DEBUG
+    Serial.println();
+#endif // SERIAL_DEBUG
 }
 
-// Helper function for logging messages with or without newline
+#ifdef SERIAL_DEBUG
+
 void DebugEsp::printLog(LogLevel level, const char *message, bool newLine)
 {
     switch (level)
@@ -68,5 +85,6 @@ void DebugEsp::printLog(LogLevel level, const char *message, bool newLine)
         Serial.print(message);
     }
 }
+#endif // SERIAL_DEBUG
 
 #endif // DEBUG_ACTIVE
