7b23adcf4e70ba7b233e87a21d9e0cf2d0d0cc77
Jakub Cihanek <jakubcihanek@gmail.com>
2024-09-24 15:19:46 +0200

    wlan config

--- src/Config.h
+++ src/Config.h
@@ -12,12 +12,13 @@
 
 // INFO Network connection related config
 
-#define NETWORK_CONNECTION_ETH
+// #define NETWORK_CONNECTION_ETH
 
 #define NETWORK_CONNECTION_WIFI
 
 #define IP_ADDRESS 10, 10, 42, 5
 #define DEFAULT_GATEWAY 10, 10, 42, 1
 #define SUBNET_MASK 255, 255, 255, 0
+#define HOSTNAME "Lora2MQTT Gateway"
 
 #endif // CONFIG_H
